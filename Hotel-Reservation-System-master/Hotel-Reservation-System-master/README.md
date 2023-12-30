0e530614a481ff63f35c21d6cda7539b1cc20407
Balajis770 <145565917+Balajis770@users.noreply.github.com>
2023-12-30 15:08:20 +0530

    Delete Hotel-Reservation-System-master/Hotel-Reservation-System-master directory

--- Hotel-Reservation-System-master/Hotel-Reservation-System-master/README.md
+++
@@ -1,2 +0,0 @@
-# Hotel-Reservation-System
-Complete mern stack project where user can choose hotel and reserve rooms online
--- Hotel-Reservation-System-master/Hotel-Reservation-System-master/server/routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginPostController, registerPostController } from "../controllers/auth.controller.js";
-const router = express.Router();
-
-router.post("/register", registerPostController);
-router.post("/login", loginPostController);
-
-export default router;
